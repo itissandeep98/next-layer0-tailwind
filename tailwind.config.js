cd5cae7cd57879e6566fd4d1821f47e144735609
Sandeep Kumar Singh <itissandeep98@gmail.com>
2022-10-20 01:18:30 +0530

    prettier and fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}'
-  ],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./layouts/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				Mulish: ["Mulish", "sans-serif"],
+				poppins: ["Poppins", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 };
